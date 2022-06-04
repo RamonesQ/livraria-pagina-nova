596dfce09a559007f72c902ae984af8be16d98c8
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-04 11:59:21 -0300

    criado o .sequelizerc e configurado

--- database/seeders/20220604141718-create-books.js
+++ database/seeders/20220604141718-create-books.js
@@ -3,32 +3,14 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Books', [{
-      title: 'Por que dormimos?',
+      title: 'Por que dormimos?IV',
       edition_year:2000,
       author:'Natalia',
       pages: 100,
       stock: 3,
       createdAt: new Date(),
       updatedAt: new Date()
-      },
-      {
-        title: 'Por que dormimos?II',
-        edition_year:2000,
-        author:'Natalia',
-        pages: 100,
-        stock: 3,
-        createdAt: new Date(),
-        updatedAt: new Date()
-        },
-        {
-          title: 'Por que dormimos? III',
-          edition_year:2000,
-          author:'Natalia',
-          pages: 100,
-          stock: 3,
-          createdAt: new Date(),
-          updatedAt: new Date()
-          }], {});
+      }], {});
   },
 
   async down(queryInterface, Sequelize) {
