4769a3e3e2878523242935ec74a7deaf07c6ad2e
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-25 12:15:03 -0300

    add fs unlink

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,12 +1,13 @@
 const { Books } = require("../database/models");
 const cloudinary = require('../config/cloudinary')
+const fs = require('fs')
 
 const imageFolder = "images/"
 const BookController = {
   async create(req, res) {
     const file = req.files[0];
    const uploadPath = await cloudinary.uplads(file.path, 'livraria')
-
+    fs.unlinkSync(file.path)  
     const newBook = await Books.create({
       ...req.body,
 
